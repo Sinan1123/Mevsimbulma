d958e0bff886ea72977700d79ccfc7bbde619897
sinan12344 <150492820+sinan12344@users.noreply.github.com>
2025-05-04 17:54:40 +0300

    Add project files.

---
+++ Mevsimbulma/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Mevsimbulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Hangi ayın mevsim bilgisini almak istiyorsanız o ayın sayısını giriniz. 1 ile 12 arasında bir sayı. (çıkmak için 0)");
+
+                int ayNumber = Convert.ToInt32(Console.ReadLine());
+                string mevsim = "";
+
+                if (ayNumber == 0) 
+                {
+                    break; 
+                }
+
+                if (ayNumber == 12 || ayNumber == 1 || ayNumber == 2)
+                {
+                    mevsim = "Kış";
+                }
+                else if (ayNumber >= 3 && ayNumber <= 5)
+                {
+                    mevsim = "İlkbahar";
+                }
+                else if (ayNumber >= 6 && ayNumber <= 8)
+                {
+                    mevsim = "Yaz";
+                }
+                else if (ayNumber >= 9 && ayNumber <= 11)
+                {
+                    mevsim = "Sonbahar";
+                }
+
+                if (ayNumber <= 0 || ayNumber >= 13)
+                {
+                    Console.WriteLine("1 ile 12 arasında bir sayı girin");
+                }
+                else
+                {
+                    Console.WriteLine(ayNumber + " -> " + mevsim);
+                }
+            }
+        }
+    }
+}
